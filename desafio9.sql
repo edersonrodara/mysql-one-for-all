e77c264695134718e8d033e64ae2037ba6f36774
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-08-03 19:48:18 -0300

    requesito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 
-CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(50), IN album VARCHAR(50))
+CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(50))
 BEGIN
 
 SELECT c.cantor artista, a.album album FROM SpotifyClone.cantores c
-INNER JOIN SpotifyClone.albuns a ON a.cantor_id = c.cantor_id ORDER BY album;
+INNER JOIN SpotifyClone.albuns a ON a.cantor_id = c.cantor_id WHERE c.cantor = artista ORDER BY album;
 
 END $$
 
