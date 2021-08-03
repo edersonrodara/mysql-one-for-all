888eb10c9b741faf93a1f9f4324829a8c6433a71
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-08-03 20:06:08 -0300

    requesito 10 fix

--- desafio10.sql
+++ desafio10.sql
@@ -3,9 +3,9 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE resultado INT;
-    SELECT count(*) FROM SpotifyClone.historico_musica WHERE usuario_id = id INTO resultado;
-    RETURN resultado;
+DECLARE resultado INT;
+SELECT count(*) FROM SpotifyClone.historico_musica WHERE usuario_id = id INTO resultado;
+RETURN resultado;
 END $$
 
 DELIMITER ;
